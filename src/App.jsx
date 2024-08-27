93d594e7b7862183a8734ec6e859a65cd3cf2e18
Pedro <pedrohenriquesb02@gmail.com>
2024-08-27 11:54:10 -0300

    Segudo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import {Outlet} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      
+      <Nav/>
+        {/* Chama todos os componentes filho */}
+        <Outlet/>
+      <Footer/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+/* Instalando o pacote de dependências para criar as rotas */
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import Error from './routes/Error.jsx'
+import Home from './routes/Home.jsx'
+import Aluno from './routes/Aluno.jsx'
 
+/* Criando a função createBrowserRouter */
+
+const router = createBrowserRouter([
+  {
+    /* Criando os elementos pai */
+    path:'/', element:<App/>,
+    errorElement:<Error/>,
+
+    // Criando os elementos filhos
+
+    children:[
+      {path:'/', element:<Home/>},
+      {path:'/aluno', element:<Aluno/>}
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Criando a desestruturação para que seja renderizada */}
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
